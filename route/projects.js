6bbf606abd2904fd4ea290bece157b9b98802af9
= <=>
2023-03-06 17:08:04 +0700

    March 6 2023 By Moc Dev

--- route/projects.js
+++ route/projects.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { mysql } = require('../Connect/conect');
+const { id } = require('../module/modulars');
 
 const queryMultipleTime = ( data, queries, index, callback ) => {
 
@@ -70,7 +71,7 @@ router.get('/', (req, res) => {
     })
 })
 
-getMultipleProjectTypeInfo = ( queries, index, data, callback ) => {
+const getMultipleProjectTypeInfo = ( queries, index, data, callback ) => {
     if( index === queries.length ){
         callback({ context: data })
     }else{
@@ -284,5 +285,62 @@ router.post('/project/:project_id/partners/and/users', (req, res) => {
     })
 });
 
+router.post('/', ( req, res ) => {
+    const { project } = req.body;
+    const{ project_name, project_master, description } = project;
+    const query = `CALL CREATE_PROJECT('${ project_name }', 'PJ${ id().toUpperCase() }', '${ project_master.credential_string }', '${ description }')`;
+    mysql( query, result => {
+        const { success, content, project_id } = result[0][0];
+        if( success ){
+            const query = `
+            SELECT * FROM PROJECTS AS P
+                INNER JOIN PROJECT_STATUS AS PS ON PS.STATUS_ID = P.PROJECT_STATUS
+                    INNER JOIN ACCOUNT_DETAIL AS AD
+                WHERE P.PROJECT_MASTER = AD.CREDENTIAL_STRING
+                    AND PROJECT_ID = ${ project_id }
+            `;
+            mysql(query, (result) => {
+                const projects = result;
+                getProjectDetailInfor( projects, 0, ({ projectDetails }) => {
+                    res.status(200).send({ success: true, project: projectDetails[0] })
+                })
+            })
+        }else{
+            res.status(500).send({ success, content });
+        }
+    })
+})
+
+router.post('/project/tasks', (req, res) => {
+    const { credential_string, project_id  } = req.body;
+
+    const query = `
+        INSERT INTO TASKS( project_id, task_owner, task_state, task_label, task_description )
+        VALUES( ${ project_id }, '${ credential_string }', 1, 'Yêu cầu mới', 'Mô tả yêu cầu' );
+    `;
+    mysql( query, result => {
+        const task_id = result.insertId;
+
+        const query = `
+            SELECT * FROM TASKS AS T
+                INNER JOIN ACCOUNT_DETAIL AS AD
+                    ON T.TASK_OWNER = AD.CREDENTIAL_STRING
+                        INNER JOIN TASK_STATUS AS TS ON TS.STATUS_ID = T.TASK_STATE
+            WHERE PROJECT_ID = ${ project_id } AND TASK_ID = ${ task_id };
+        `;
+
+        mysql( query, result => {
+            const task = result[0];
+            res.send({ success: true, task })
+        })
+    })
+})
+
+router.put('/project/task', (req, res) => {
+    const { changes, credential_string, task_id } = req.body;
+    console.log({ changes, credential_string, task_id })
+
+    res.status(200).send({ success: true })
+})
 
 module.exports = router;
