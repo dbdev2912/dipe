d17dbb96e77f578b4b2f1100c25a0c40865edc1c
= <=>
2023-03-07 17:03:29 +0700

    Mar 7 2023

--- src/cpn/client/index.js
+++ src/cpn/client/index.js
@@ -1,7 +1,8 @@
 import Home from './splitter';
 import { Projects, SuProjects, SuProject } from './projects';
 import { SuUsers, SuUser } from './users';
+import { Version } from './version';
 
 export{
-    Home, Projects, SuProjects,  SuProject, SuUsers, SuUser
+    Home, Projects, SuProjects,  SuProject, SuUsers, SuUser, Version
 }
